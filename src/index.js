ef4691dbb812c243737aac1af4193a43cd7b3b1a
Ntare cedrick <ntarecedrick250@gmail.com>
2022-12-27 08:48:27 +0200

    hide the mongodb url

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import mongoose from 'mongoose';
-import express from 'express'
+import express from 'express';
+import dotenv from 'dotenv'
 import router from './routes/router';
 import authRoutes from './routes/auth';
+
+// dotenv.config()
 const app = express();
 app.use(express.json({limit: '500mb'}))
-// app.use(express.urlencoded({limit:'500mb'}))
+
 
 try {
     mongoose.set('strictQuery', false)
-    mongoose.connect("mongodb://localhost:27017/serverDB", { useNewUrlParser: true });
+    mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true });
     app.use("/api", router);
     app.use('/user', authRoutes);
     app.listen(2002, () => {
